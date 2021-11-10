45c5162ebb1aa5941643b29cf5b411448fcea914
Sonalpt <remsbdj@gmail.com>
2021-11-10 16:38:39 +0100

    accordion feature added (without auto-close previous accordion element)

---
+++ index.js
@@ -0,0 +1,37 @@
+var question = document.getElementsByClassName("question");
+var answer = document.getElementsByClassName("answer");
+var i;
+
+
+$('.question').click(function(){
+
+    
+    $(this).toggleClass('active');
+    $(this).next().toggleClass('show');
+
+    if( $(this).hasClass('active') ){
+      $(this).removeClass('active');
+      $(this).next().removeClass('show');
+    }else{
+      $('this').removeClass('active');
+      $('.answer').removeClass('show');
+  
+      $(this).toggleClass('active');
+      $(this).next().addClass('show');
+    }
+  
+  })
+
+
+
+for (i = 0; i < question.length; i++) {
+    question[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var answer = this.nextElementSibling;
+      if (answer.style.maxHeight) {
+        answer.style.maxHeight = null;
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+      }
+    });
+  }
